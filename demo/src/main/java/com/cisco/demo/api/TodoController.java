996da0e2e764cba8f006023b07480b156e2790af
sovisrushain <sovisrushain@gmail.com>
2024-07-19 22:47:49 +0530

    create todo controller

---
+++ demo/src/main/java/com/cisco/demo/api/TodoController.java
@@ -0,0 +1,9 @@
+package com.cisco.demo.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/todos")
+public class TodoController {
+}
